c905a518778d7689ba455243ee0ce5f69d131619
NYPD <nypd54@outlook.com>
2018-03-29 22:50:14 -0400

    Add basic ajax error handling
    Closes #8

--- WebContent/js/global.js
+++ WebContent/js/global.js
@@ -38,4 +38,46 @@ if(subscribeButton !== null) {
     });
     
   });
+};
+
+$(document).ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
+  
+  $allModals.modal('hide');
+  
+  var isServerError = jqXHR.status >= 500 && jqXHR.status < 600;
+  var isUnauthorized = jqXHR.status === 401;
+  
+  if(isUnauthorized) {
+    window.location.href = window.location.origin + '/admin?prevPath=' + window.location.pathname; 
+    return;
+  }
+  
+  if(isServerError)
+    $modalSmall.find('.modal-dialog').html(jqXHR.responseText);
+  else
+    $modalSmall.find('.modal-dialog').html(getGenericErrorModal());
+  
+  $modalSmall.modal('show');
+  
+});
+
+
+function getGenericErrorModal() {
+  
+  return '<div class="modal-content">' +
+
+            '<div class="modal-header">' +
+              '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+              '<h4 class="modal-title">Error</h4>' +
+            '</div>' +
+            
+            '<div class="modal-body">' +
+             '<p>Something is goofed up. If this keeps happening please submit a bug report.</p>' +
+            '</div>' +
+            
+            '<div class="modal-footer">' +
+              '<button type="button" class="btn btn-warning" data-dismiss="modal">OK</button>' +
+            '</div>' +
+            
+          '</div>';
 }
