00b6e7d3a4ee85644ee1b8d98e0d5d20d35c1956
NYPD <nypd54@outlook.com>
2018-04-23 18:39:57 -0400

    Remove unused static string

--- src/live/soilandpimp/util/AppConstants.java
+++ src/live/soilandpimp/util/AppConstants.java
@@ -7,7 +7,6 @@ public class AppConstants {
     public static final String DEVELOPMENT_PROFILE = "DEVELOPMENT";
     public static final String TEST_PROFILE = "TEST";
     public static final String PRODUCTION_PROFILE = "PRODUCTION";
-    public static final String CASSANDRA_KEYSPACE = "soilandpimp";
 
     public static final String REMEBER_ME_PARAMETER_NAME = "remember_me";
     public static final String API_TYPE_COOKIE_NAME = "apiType";
