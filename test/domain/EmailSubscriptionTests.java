aade26751d4d9e06f0c721b7e31bae0a408b5797
NYPD <6412879+NYPD@users.noreply.github.com>
2021-06-05 12:20:18 -0600

    Remove unneeded dependencies

--- test/domain/EmailSubscriptionTests.java
+++ test/domain/EmailSubscriptionTests.java
@@ -1,8 +1,5 @@
 package domain;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
-
 import org.junit.Test;
 
 import live.soilandpimp.domain.EmailSubscription;
@@ -13,9 +10,6 @@ public class EmailSubscriptionTests {
     public void shouldContsturctNewEmailSubscription() {
         String email = "a@a.a";
         new EmailSubscription(email);
-
-        System.out.println(new BigInteger(130, new SecureRandom()).toString(32));
-
     }
 
     @Test(expected = IllegalArgumentException.class)
